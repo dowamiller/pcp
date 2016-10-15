065740aa18d5f9352062a3ce29d659fe0902df50
Dominik Waller <dowamiller@gmail.com>
2016-10-15 13:01:10 +0200

    Pong spielbar

--- pcp_ueb2/Teil1.js
+++ pcp_ueb2/Teil1.js
@@ -4,23 +4,42 @@
 // Uebung 2: WebGL
 //
 window.onload = startup;
-
-
-
+window.requestAnimationFrame(drawAnimated);
 // canvas and gl object are saved globally
 var canvas;
 var gl;
 var shaderProgram;
 var aVertexPositionId;
 var posBuffer;
-var aColorId;
-var colorBuffer;
-var cirlceVerts;
-var cirlceColors;
-var rotInRadians = 0;
-var uRotMatrixId;
-var uScaleMatrixId;
-var start = null;
+var uColorId;
+var uModelViewMatrixId;
+var matrixStack = [];
+
+// middel line
+var numberOfVertsLine = 2;
+
+//Padel
+var padelWidth = 15;
+var padelHeight = 100;
+var numberOfVertsPadel = 6;
+var padelDirection = 0; // -1: up, 1: down
+var padelSpeed = 0.8; // [pixel / ms]
+var padel1Position;
+var padel2Position;
+
+//Ball;
+var ballWidth = 12;
+var ballHeight = 12;
+var numberOfVertsBall = 6;
+var ballDirection;
+var ballSpeed=0.5; // [pixel/ms]
+var ballPosition;
+
+//PointCounter;
+var numberOfVertsPointCounter;
+
+//animation
+var oldTime = null;
 
 /**
  * Startup function to be called when the body is loaded
@@ -36,8 +55,9 @@ function startup() {
 function initGL() {
     "use strict";
     shaderProgram = loadAndCompileShaders(gl, 'VertexShader.glsl', 'FragmentShader.glsl');
-    gl.clearColor(1,1,1,1);
+    gl.clearColor(0,0,0,1);
     setUpAttributes();
+    setUpPositions();
     setUpUniforms();
     setUpBuffers();
 }
@@ -45,96 +65,162 @@ function initGL() {
 function setUpAttributes(){
     "use strict";
     aVertexPositionId = gl.getAttribLocation(shaderProgram , 'aVertexPosition');
-    aColorId = gl. getAttribLocation(shaderProgram,'aColor');
 }
 
 function setUpUniforms(){
-    uRotMatrixId = gl.getUniformLocation(shaderProgram, 'uRotMatrix');
-    uScaleMatrixId = gl.getUniformLocation(shaderProgram, 'uScaleMatrix');
+    uModelViewMatrixId = gl.getUniformLocation(shaderProgram, 'uModelViewMatrix');
+    uColorId = gl.getUniformLocation(shaderProgram, 'uColorId');
 }
 
 function setUpBuffers(){
     "use strict";
-    constructCircle();
-    colorCircle();
     posBuffer = gl.createBuffer();
-    colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, cirlceVerts, gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, cirlceColors, gl.STATIC_DRAW);
+    var verts = setUpVerts();
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+}
+function setUpPositions(){
+    padel1Position = [50, canvas.height/2,0];
+    padel2Position = [canvas.width - 50, canvas.height/2,0];
+    ballPosition = [canvas.width/2, canvas.height/2, 0];
+    ballDirection = [0, 0, 0];
+}
+
+function setUpVerts(){
+    var verts = [];
+    verts = verts.concat([
+        canvas.width/2, 0,
+        canvas.width/2, canvas.height
+    ]);
+    verts = verts.concat([
+        -padelWidth/2, -padelHeight/2,
+        padelWidth/2, -padelHeight/2,
+        padelWidth/2, padelHeight/2,
+        padelWidth/2, padelHeight/2,
+        -padelWidth/2, padelHeight/2,
+        -padelWidth/2, -padelHeight/2
+    ]);
+    verts = verts.concat([
+        -ballWidth/2, -ballHeight/2,
+        ballWidth/2, -ballHeight/2,
+        ballWidth/2, ballHeight/2,
+        ballWidth/2, ballHeight/2,
+        -ballWidth/2, ballHeight/2,
+        -ballWidth/2, -ballHeight/2
+    ]);
+    return verts;
 }
 
 function draw() {
     "use strict";
     gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.lineWidth(100);
     gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
     gl.vertexAttribPointer ( aVertexPositionId , 2, gl.FLOAT , false , 0, 0);
     gl.enableVertexAttribArray ( aVertexPositionId );
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.vertexAttribPointer( aColorId , 4 , gl.FLOAT,false,0,0);
-    gl.enableVertexAttribArray (aColorId);
-    var rotMatrix = makeRotation(rotInRadians);
-    var scaleMatrix = makeScale(canvas.height / canvas.width,1);
-    gl.uniformMatrix3fv(uRotMatrixId, false, rotMatrix);
-    gl.uniformMatrix3fv(uScaleMatrixId, false, scaleMatrix);
-    gl.drawArrays (gl. TRIANGLE_FAN ,0 ,362);
+    gl.uniform4fv(uColorId,[1,1,1,1]);
 
-}
+    //orthoprojectcion
+    var matrix = mat4.create();
+    mat4.ortho(matrix, 0, canvas.width, canvas.height, 0, 0, 0);
+    matrixStack.push(matrix);
 
-function constructCircle(){
-    cirlceVerts = new Float32Array(362*2);
-    var i = 0;
-    var deg = 0;
-    cirlceVerts[i++] = 0;
-    cirlceVerts[i++] = 0;
-    do {
-        cirlceVerts[i++] = Math.cos((Math.PI / 180) * deg);
-        cirlceVerts[i++] = Math.sin((Math.PI / 180) * deg);
-        deg++;
-    } while(i<(362*2)-1)
-}
+    //drawMiddelLine
+    gl.uniformMatrix4fv( uModelViewMatrixId ,false, matrixStack[matrixStack.length-1]);
+    gl.drawArrays (gl. LINES ,0 ,numberOfVertsLine);
 
-function colorCircle(){
-    cirlceColors = new Float32Array(362*4);
-    var deg = 0;
-    var i = 0;
-    cirlceColors[i++]= 1;
-    cirlceColors[i++]= 1;
-    cirlceColors[i++]= 1;
-    cirlceColors[i++]= 1;
-    do {
-        cirlceColors[i++]= 0;
-        cirlceColors[i++]= Math.sin((Math.PI / 180)*deg);
-        cirlceColors[i++]= 0 ;
-        cirlceColors[i++]= 1;
-        deg = deg+2;
-    }while(i<(362*4)-1)
-}
+    //draw padel 1
+    matrix = mat4.clone(matrixStack[matrixStack.length-1]);
+    mat4.translate(matrix, matrix,padel1Position);
+    gl.uniformMatrix4fv( uModelViewMatrixId ,false, matrix);
+    gl.drawArrays (gl. TRIANGLES ,numberOfVertsLine ,numberOfVertsPadel);
 
-function makeRotation(angleInRadians) {
-    var c = Math.cos(angleInRadians);
-    var s = Math.sin(angleInRadians);
-    return [
-        c, -s, 0,
-        s, c, 0,
-        0, 0, 1
-    ];
-}
+    //draw padel 2
+    matrix = mat4.clone(matrixStack[matrixStack.length-1]);
+    mat4.translate(matrix, matrix,padel2Position);
+    gl.uniformMatrix4fv( uModelViewMatrixId ,false, matrix);
+    gl.drawArrays (gl. TRIANGLES ,numberOfVertsLine ,numberOfVertsPadel);
 
-function makeScale(sx, sy) {
-    return [
-        sx, 0, 0,
-        0, sy, 0,
-        0, 0, 1
-    ];
+    //drawBall
+    matrix = mat4.clone(matrixStack[matrixStack.length-1]);
+    mat4.translate(matrix, matrix, ballPosition);
+    gl.uniformMatrix4fv( uModelViewMatrixId ,false, matrix);
+    gl.drawArrays (gl. TRIANGLES, numberOfVertsLine + numberOfVertsPadel, numberOfVertsBall);
 }
 
 function drawAnimated ( timeStamp ) {
-    if(!start) start = timeStamp;
-    var progress = timeStamp - start;
-    rotInRadians =(2 * Math.PI / 7) * (progress) / 1000;
-    draw();
+    if(oldTime)
+    {
+        var delta = timeStamp - oldTime;
+        oldTime = timeStamp;
+        calculateNewPadelPosition(delta);
+        calculateNewBallPosition(delta);
+        draw();
+    } else {
+        oldTime = timeStamp;
+    }
     window.requestAnimationFrame(drawAnimated);
 }
-window.requestAnimationFrame ( drawAnimated );
+//window.requestAnimationFrame(drawAnimated);
+
+function calculateNewPadelPosition(delta){
+    var newY = padel1Position[1] + delta  * padelSpeed * padelDirection;
+    if(newY < ((padelHeight/2) -1)) {
+        padel1Position[1] = padelHeight / 2 - 1;
+    } else if (newY > canvas.height - (padelHeight/2)){
+        padel1Position[1] = canvas.height - (padelHeight/2);
+    } else {
+        padel1Position[1] = newY;
+    }
+    if(ballPosition[1] < padelHeight / 2 ) {
+        padel2Position[1] = padelHeight / 2 - 1;
+    } else if(ballPosition[1] > canvas.height - padelHeight/2 ){
+        padel2Position[1] = canvas.height - padelHeight/2
+    } else {
+            padel2Position[1] = ballPosition[1];
+    }
+}
+
+function calculateNewBallPosition(delta){
+    var newX = ballPosition[0] + delta *ballDirection[0];
+    var newY = ballPosition[1] = ballPosition[1] + delta *ballDirection[1];
+    if (newY < 0 || newY > canvas.height){
+        ballDirection[1] = -1 * ballDirection[1];
+    }
+    if((newX > padel1Position[0]-padelWidth/2 && newX < padel1Position[0]+padelWidth/2) && (newY > padel1Position[1] - padelHeight/2 && newY < padel1Position[1] + padelHeight/2)){
+        ballDirection[0] = -1 * ballDirection[0];
+    }   else if((newX > padel2Position[0]-padelWidth/2 && newX < padel2Position[0]+padelWidth/2) && (newY > padel2Position[1] - padelHeight/2 && newY < padel2Position[1] + padelHeight/2)){
+        ballDirection[0] = -1 * ballDirection[0];
+    }
+    ballPosition[0] = ballPosition[0] + delta *ballDirection[0];
+    ballPosition[1] = ballPosition[1] + delta *ballDirection[1];
+    if(newX < 0 || newX > canvas.width){
+        if(newX < 0){
+            // Player1 -1 Point
+        } else {
+            //player2 -1Point
+        }
+        ballPosition = [canvas.width/2, canvas.height/2, 0];
+        ballDirection = [0, 0, 0];
+    }
+}
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode == 38) {
+        padelDirection = -1;
+    }
+    if (event.keyCode == 40) {
+        padelDirection = 1;
+    }
+    if (event.keyCode == 32) {
+        var randAngle = Math.random()*2*Math.PI;
+        ballDirection = [ballSpeed*Math.sin(randAngle), ballSpeed*Math.cos(randAngle), 0];
+    }
+})
+
+window.addEventListener("keyup", function(event) {
+    if (event.keyCode == 38 || event.keyCode == 40) {
+        padelDirection = 0;
+    }
+})
+
--- pcp_ueb2/VertexShader.glsl
+++ pcp_ueb2/VertexShader.glsl
@@ -1,10 +1,8 @@
 attribute vec2 aVertexPosition;
 attribute vec4 aColor;
-uniform mat3 uRotMatrix;
-uniform mat3 uScaleMatrix;
-varying vec4 vColor;
+uniform mat4 uModelViewMatrix ;
+
 
 void main() {
-    gl_Position = vec4((uScaleMatrix * uRotMatrix * vec3(aVertexPosition,0)),1);
-    vColor = aColor;
+    gl_Position = uModelViewMatrix * vec4(aVertexPosition,0,1);
 }
